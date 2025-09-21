1053afee5903eaf265f6ab9a5cd23d5709fbaff0
jsampson35 <jsampson35@gatech.edu>
2025-09-21 16:02:33 -0400

    design document, scripts to build and run the project, and updated .gitignore

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
 # Write a script to build your optimizer in this file 
 # (As required by your chosen optimizer language)
-
 mkdir -p build
-find src -name "*.java" > sources.txt
-javac -d build @sources.txt
+find materials/src -name "*.java" > sources.txt -exec javac -d build -sourcepath materials/src {} + 
+# or just /
+# find $PWD/materials -name "*.java" > sources.txt
+
+#javac -d build @sources.txt
+
+#how to deal with the space in my JOSHUA SAMPSON
--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # Write a script to run your optimizer in this file 
 # This script should take one command line argument: an path to 
 # an input ir file as 
 # This script should output an optimized ir file named "out.ir"
-
-java -cp build Demo $1 $2
+java -cp build Demo "$1" "$2"
